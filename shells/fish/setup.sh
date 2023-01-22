3548d61340346b852c65f1a2a2b3200e03d4ac1e
Manuel Isava <m.isava@m1.com>
2023-01-22 12:42:41 -0600

    Fix fish setup script

--- shells/fish/setup.sh
+++ shells/fish/setup.sh
@@ -3,22 +3,24 @@
 # Install fish shell
 brew install fish
 
-# Copy core fish functions
-cp -R ./functions/* ~/.config/fish/functions
-
 # Make fish the login shell
 echo $(which fish) | sudo tee -a /etc/shells
 chsh -s $(which fish)
 
 # Install Fisher (fish plugin manager)
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 
 # Install fish ssh-agent plugin
-fisher install danhper/fish-ssh-agent
+fish -c "fisher install danhper/fish-ssh-agent"
+
+# Copy core fish functions
+cp -R ./functions/* ~/.config/fish/functions
 
 # Install SDKMAN! (SDK pkg manager)
 curl -s "https://get.sdkman.io" | bash
 
 # Install SDKMAN fish plugin
-fisher install reitzig/sdkman-for-fish@v1.4.0
+fish -c "fisher install reitzig/sdkman-for-fish@v1.4.0"
 
+# Switch to fish shell
+exec fish
